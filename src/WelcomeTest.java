0669b5f7ef145977e9fcea34b3c03a37d986c6a9
dakhamzadeh <dakhamzadeh@csumb.edu>
2018-04-19 00:34:54 -0700

    Update WelcomeTest.java

--- src/WelcomeTest.java
+++ src/WelcomeTest.java
@@ -1,4 +1,16 @@
+import static org.junit.Assert.*;
+
 
 public class WelcomeTest {
+  
+     String message = "Welcome to the Land of Hurkle";	
+	   Welcome messageUtil = new Welcome(message);
+	   
+  
+  @Test
+	   public void testPrintMessage() {
+	      assertEquals(message,messageUtil.printMessage());
+
+}
 
 }
